0caacdaff4db8ce8583ed1c712385df83c500b87
Arnaud Vazard <arnaud.vazard@tecofrance.com>
2015-07-24 17:23:19 +0200

    Changed the signature of the cmdProcessCallback functions to take an irc event as first parameter

--- core/bot.go
+++ core/bot.go
@@ -16,7 +16,7 @@ type Bot struct {
 	ChannelKey        string
 	ircConn           *irc.Connection
 	msgHandlers       []func(string, func(string))
-	cmdHandlers       []func(string, func(string)) bool
+	cmdHandlers       []func(*irc.Event, func(string)) bool
 	msgReplyCallbacks []func(string)
 	cmdReplyCallbacks []func(string)
 }
@@ -42,7 +42,7 @@ func (bot *Bot) AddMsgHandler(msgProcessCallback func(string, func(string)), rep
 // replyCallback is to be called by cmdProcessCallback (or not) to yield and process its result as a string message.
 // cmdProcessCallback must check if their replyCallback is nil before using it
 // Command handlers must return true if they found a command to process, false otherwise
-func (bot *Bot) AddCmdHandler(cmdProcessCallback func(string, func(string)) bool, replyCallback func(string)) {
+func (bot *Bot) AddCmdHandler(cmdProcessCallback func(*irc.Event, func(string)) bool, replyCallback func(string)) {
 	if cmdProcessCallback != nil {
 		bot.cmdHandlers = append(bot.cmdHandlers, cmdProcessCallback)
 		bot.cmdReplyCallbacks = append(bot.cmdReplyCallbacks, replyCallback)
@@ -59,7 +59,7 @@ func (bot *Bot) ReplyToAll(message string) {
 
 func (bot *Bot) mainHandler(event *irc.Event) {
 	for i, handler := range bot.cmdHandlers {
-		if handler(event.Message(), bot.cmdReplyCallbacks[i]) {
+		if handler(event, bot.cmdReplyCallbacks[i]) {
 			return
 		}
 	}
