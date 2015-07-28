3464cc501dddd0a846888fa4eb58476c49b5b52f
Arnaud V <vaz-git@gmx.com>
2015-07-28 20:39:33 +0200

    Memo: Added usage message + refactored part of the code

--- memo/memo.go
+++ memo/memo.go
@@ -31,35 +31,43 @@ func Init(db *sql.DB) {
 }
 
 func HandleMemoCmd(event *irc.Event, callback func(string)) bool {
-	message := strings.TrimSpace(event.Message())
-	if strings.HasPrefix(message, "!memo ") {
-		user_to := strings.Split(message, " ")[1]
-		user_from := event.Nick
-		message = strings.TrimSpace(strings.TrimPrefix(message, "!memo "+user_to))
-		if message == "" {
-			return false
-		}
-
-		sqlStmt := "INSERT INTO Memo (user_to, user_from, message) VALUES ($1, $2, $3)"
-		_, err := _database.Exec(sqlStmt, user_to, user_from, message)
-		if err != nil {
-			log.Fatalf("%q: %s\n", err, sqlStmt)
-		}
-
-		if callback != nil {
-			callback(fmt.Sprintf("Memo for %s from %s: \"%s\"", user_to, user_from, message))
-		}
-		return true
+	fields := strings.Fields(event.Message())
+	// fields[0]  => Command
+	// fields[1]  => recipient's nick
+	// fields[2:] => message
+	if len(fields) == 0 || fields[0] != "!memo" {
+		return false
 	}
-	return false
+
+	if len(fields) < 3 {
+		if callback != nil {
+			callback(fmt.Sprintf("Memo usage: %s \"recipient's nick\" \"message\"", fields[0]))
+		}
+		return false
+	}
+	user_from := event.Nick
+	message := strings.Join(fields[2:], " ")
+
+	sqlStmt := "INSERT INTO Memo (user_to, user_from, message) VALUES ($1, $2, $3)"
+	_, err := _database.Exec(sqlStmt, fields[1], user_from, message)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, sqlStmt)
+	}
+
+	if callback != nil {
+		callback(fmt.Sprintf("Memo for %s from %s: \"%s\"", fields[1], user_from, message))
+	}
+	return true
+
 }
 
 func SendMemo(event *irc.Event, callback func(string)) bool {
 	if callback == nil {
 		log.Println("Callback nil for the SendMemo function, unable to send the memo")
 	}
+
 	user := event.Nick
-	sqlQuery := "SELECT id, user_to, user_from, message, strftime('%d/%m/%Y %H:%M', datetime(date, 'localtime')) FROM Memo WHERE user_to = $1;"
+	sqlQuery := "SELECT id, user_to, user_from, message, strftime('%d/%m/%Y @ %H:%M', datetime(date, 'localtime')) FROM Memo WHERE user_to = $1;"
 	rows, err := _database.Query(sqlQuery, user)
 	if err != nil {
 		log.Fatalf("%q: %s\n", err, sqlQuery)
@@ -78,7 +86,7 @@ func SendMemo(event *irc.Event, callback func(string)) bool {
 	for rows.Next() {
 		rows.Scan(&id, &user_to, &user_from, &message, &date)
 		idList = append(idList, id)
-		callback(fmt.Sprintf("%s: %s left you a memo : \"%s\" (%s)", user_to, user_from, message, date))
+		callback(fmt.Sprintf("%s: %s left you a memo => \"%s\" (%s)", user_to, user_from, message, date))
 	}
 	rows.Close()
 
