6425f87ef94810757f64ad57a92dc46e7630fe3e
Arnaud Vazard <vaz-git@gmx.com>
2015-08-14 12:21:50 +0200

    Help: Added comments

--- help/help.go
+++ help/help.go
@@ -3,6 +3,8 @@
 // Copyright (c) 2015 Arnaud Vazard
 //
 // See LICENSE file.
+
+// Help package
 package help
 
 import (
@@ -13,12 +15,14 @@ import (
 
 var messageList []string
 
+// Store all the messages to display via the help command
 func Init(helpMessages ...string) {
 	for _, message := range helpMessages {
 		messageList = append(messageList, message)
 	}
 }
 
+// Handler for the !help command
 func HandleHelpCmd(event *irc.Event, callback func(*core.ReplyCallbackData)) bool {
 	fields := strings.Fields(event.Message())
 	if len(fields) == 0 || fields[0] != "!help" {
