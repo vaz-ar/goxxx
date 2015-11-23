92eeaa3994a5100908bab1e6b5a8b8b64c739566
Arnaud Vazard <vaz-git@gmx.com>
2015-11-23 14:06:12 +0100

    Quote: Add first test (ref #7)

---
+++ quote/quote_test.go
@@ -0,0 +1,21 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Arnaud Vazard
+//
+// See LICENSE file.
+package quote
+
+import (
+	"testing"
+)
+
+func Test_prepareForSearch(t *testing.T) {
+
+	str := `This IS a; string.with, punctuation, quotes',		and upper: "case"  `
+	expectedResult := "this is a string with punctuation quotes and upper case"
+	result := prepareForSearch(str)
+	if result != expectedResult {
+		t.Errorf("Test result differ from expected result: \n Test result:\t%#v\nExpected result: %#v\n\n", result, expectedResult)
+	}
+
+}
