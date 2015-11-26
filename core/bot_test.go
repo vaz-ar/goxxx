92d91a6ea934ea5f920dd0c1a31d7bd082758c33
Arnaud Vazard <vaz-git@gmx.com>
2015-11-26 15:37:59 +0100

    Core: Add first test (ref #7)

---
+++ core/bot_test.go
@@ -0,0 +1,42 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Arnaud Vazard
+//
+// See LICENSE file.
+
+/*
+Package quote contains quote commands
+*/
+package core
+
+import (
+	"github.com/thoj/go-ircevent"
+	"testing"
+)
+
+func Test_GetTargetFromEvent(t *testing.T) {
+
+	// Channel
+	event := irc.Event{
+		Nick:      "Sender",
+		Arguments: []string{"#test_channel", "test message"}}
+	expectedResult := "#test_channel"
+
+	result := GetTargetFromEvent(&event)
+
+	if result != expectedResult {
+		t.Errorf("Result not matching expected result (%q != %q)", result, expectedResult)
+	}
+
+	// Nick
+	event = irc.Event{
+		Nick:      "Sender",
+		Arguments: []string{"Receiver", "test message"}}
+	expectedResult = "Sender"
+
+	result = GetTargetFromEvent(&event)
+
+	if result != expectedResult {
+		t.Errorf("Result not matching expected result (%q != %q)", result, expectedResult)
+	}
+}
